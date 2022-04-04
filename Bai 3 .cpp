7002c9cb1551a3d10997c47905b91d278b92a573
luong-dhti15a6 <102969949+luong-dhti15a6@users.noreply.github.com>
2022-04-04 20:38:03 +0700

    Add files via upload

---
+++ Bai 3 .cpp
@@ -0,0 +1,54 @@
+﻿//Câu 3: Xây dựng lớp đa thức bậc 3 DaThuc3 gồm các thuộc tính hệ số bậc 3, bậc 2, bậc 1 và bậc 0 lần lượt là a, b, c, d(a, b, c, d nguyên) là các thuộc tính riêng.
+// Trong lớp có định nghĩa các hàm công cộng sau :
+//-Hàm khởi tạo với các tham số có giá trị ngầm định bằng 0
+//- Hàm hủy
+//- Hàm hiển thị đa thức ra màn hình ở dạng(ax3 + bx2 + cx + d)
+//- Hàm toán tử “ + ”, “ - ” để tính tổng, hiệu 2 đa thức
+//Viết chương trình nhập 2 đa thức và in ra màn hình tổng, hiệu 2 đa thức đó.
+
+#include<iostream>
+using namespace std;
+
+
+class dathuc {
+private: 
+	int a, b, c, d,x;
+public:
+ dathuc (int a,int b ,int c,int d,int x) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->x = x;
+		cout<<"Bieu thuc: " << a << "x^3 - " << b << " x^2 + " << c << "x + "<<d << endl;
+	}
+	
+	
+	int tinh(int a, int b, int c, int d, int x) {
+		int giatri = 0;
+		giatri = a * pow(x,3) - b *pow(x,2) + c * x + d;
+		return giatri;
+	}
+
+	~dathuc() {
+		
+	}
+
+};
+int main() {
+	int a, b, c, d, h, k, m, n , x;
+	cout << "Nhap x: "; cin >> x;
+	cout<<"\nNhap hs da thuc 1: "; cin >> a >> b >> c >> d;
+	dathuc s1(a, b, c, d,x);
+	
+	cout << "Nhap da thuc 2: "; cin >> h >> k >> m >> n;
+	dathuc s2(h, k, m, n, x);
+
+	cout <<endl <<"tong 2 da thuc la: " << s1.tinh(a, b, c, d, x) + s2.tinh(h, k, m, n, x) << endl;
+	cout << "\nHieu 2 da thuc la: " << s1.tinh(a, b, c, d, x) - s2.tinh(h, k, m, n, x) << endl;
+	
+	system("pause");
+
+	return 0;
+
+}
---
+++ Bài 1.cpp
@@ -0,0 +1,88 @@
+﻿/*Câu 1: Xây dựng lớp khách hàng KH gồm các thuộc tính họ tên ht(kiểu char[30]), ngày sinh ns(kiểu struct NS(ngày, tháng, năm))
+, số chứng minh thư cmt(kiểu char[10]), hộ khẩu k(kiểu char[50]) là các thuộc tính riêng.
+ Trong lớp có định nghĩa các hàm công cộng sau :
+-Hàm toán tử nhập >> đi với cin để nhập dữ liệu cho các thông tin trên của 1 khách hàng
+- Hàm toán tử xuất << đi với cout để xuất các thông tin trên của khách hàng ra màn hình
+- Hàm toán tử “ > ” so sánh 2 khách hàng(khách hàng lớn hơn  là khách hàng có tuổi lớn hơn)
+Viết chương trình nhập vào n(n nguyên dương nhập từ bàn phím) khách hàng.In ra màn hình :
+-Danh sách các khách hàng có hộ khẩu “Hà Nội”.
+- Danh sách khách hàng đã sắp xếp theo thứ tự giảm dần của tuổi.*/
+
+
+#include<iostream>
+
+using namespace std;
+class khachhang {
+private:
+	char ht[30];
+	struct ns {
+		int ngay;
+		int thang;
+		int nam;
+	};
+	char cmt[10];
+	char hk[50];
+public:  
+	
+	friend void nhap1(khachhang::ns o[], int n) {
+		for (int i = 0; i < n; i++) {
+			cout << "Nhap thong tin khach hang thu " << i + 1 << endl;
+			cout << "\nNhap ngay sinh: "; cin >> o[i].ngay;
+			cout << "\nNhap thang sinh: "; cin >> o[i].thang;
+			cout << "\nNhap nam sinh: "; cin >> o[i].nam;
+		}
+	}
+   friend void nhap (khachhang kh[], int n) {
+	   for (int i = 0; i < n; i++) {
+		   cin.ignore();
+		   cout << "Nhap ten: ";
+		   cin.getline(kh[i].ht, 29);
+
+		   cout << "\nNhap so cmt: ";
+		   cin.getline(kh[i].cmt, 10);
+
+		   cout << "\nNhap ho khau: ";
+		   cin.getline(kh[i].hk, 50);
+	   }
+
+	}
+	
+
+	friend void xuat(khachhang kh[], int n) {
+		for (int i = 0; i < n; i++) {
+			cout << "Nhap thong tin khach hang thu " << i + 1 << endl;
+			cout << "======Thong tin khach hang =====" << endl;
+			cout << "|  Ngay thang nam sinh: " << o[i].ngay << "/" << o[i].thang << "/" << o[i].nam << endl;
+			cout << "|  Ho va ten: " << kh[i].ht << endl;
+			cout << "|  So cmt: " << kh[i].cmt << endl;
+			cout << "| Ho khau: " << kh[i].hk << endl;
+		}
+	}
+	friend void  HN(khachhang a[], int n) {
+		for (int i = 0;i<n;i++){
+			if (strcmp(a[i].hk, "Ha Noi") == 0) {
+				cout << a[i].ht << endl;
+				
+			}
+		}
+	}
+	
+	
+	
+};
+int main() {
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	khachhang kh[100];
+	nhap(kh,n);
+
+	xuat(kh,n);
+	
+	cout << "Cac Khach hang tai Ha Noi: " << endl;
+	HN(kh, n);
+
+	system("pause");
+	return 0;
+}
+
